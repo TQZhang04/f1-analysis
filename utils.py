a0858f6a8204467cd41cb37c8fc6ddff1cc76666
TQ Zhang <tianqiz04@gmail.com>
2024-03-08 16:02:38 -0800

    Data Cleaning: Initial
    looked through datasets to get a better sense of them, replacing "nully" values with NaN

---
+++ utils.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+# datavis
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+from pathlib import Path
+from scipy import stats
+import os
+import io
+
